6febb51cd9bec890f6d6792a6071187887ea2a4b
Wahidul30 <wahidul505@gmail.com>
2022-05-22 01:04:05 +0600

    initially setup

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+// middleware 
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Started');
+});
+
+app.listen(port, () => console.log('Listening to Craft Owl at', port));
