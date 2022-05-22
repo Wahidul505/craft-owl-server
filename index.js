658a51fbd00f5087639934a387c1f6de56dab492
Wahidul30 <wahidul505@gmail.com>
2022-05-23 01:29:05 +0600

    implemented funtionalities for signing jwt and storing user into database

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
@@ -8,6 +9,35 @@ const port = process.env.PORT || 5000;
 // middleware 
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ox5bc.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        client.connect();
+        const toolCollection = client.db('craft-owl').collection('tools');
+        const userCollection = client.db('craft-owl').collection('users');
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email }
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    email: email
+                }
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res.send({ result, token });
+        })
+    }
+    finally { }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Started');
